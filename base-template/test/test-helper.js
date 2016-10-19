a34a1f9f8acc3f27c5f8e2d46f060af49b383b04
Gilbert <gilbertbgarza@gmail.com>
2016-10-18 22:20:16 -0500

    Allow pending it_

--- base-template/test/test-helper.js
+++ base-template/test/test-helper.js
@@ -52,6 +52,6 @@ var Bluebird = require('bluebird')
 
 global.before_ = function (f) { before ( Bluebird.coroutine(f) ) }
 global.beforeEach_ = function (f) { beforeEach ( Bluebird.coroutine(f) ) }
-global.it_ = function (description, f) { it ( description, Bluebird.coroutine(f) ) }
+global.it_ = function (description, f) { it ( description, f && Bluebird.coroutine(f) ) }
 global.xit_ = function (description, f) { xit ( description, f ) }
 global.it_.only = function (description, f) { it.only( description, Bluebird.coroutine(f) ) }
