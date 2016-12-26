d0ef0436c57bb5895ba7fde493704d3482fe77b1
Gilbert <gilbertbgarza@gmail.com>
2016-12-25 23:17:55 -0600

    Workaround the npm problem

--- commands/new-project.js
+++ commands/new-project.js
@@ -4,6 +4,11 @@ var Path = require('path')
 module.exports = function createProject (vfs, baseConfig, projectName) {
   var dest = Path.resolve(baseConfig.cwd, projectName)
   vfs.copyTpl( $('base-template/{**,.*}'), dest, baseConfig )
+
+  //
+  // npm short-sightedly renames a publish .gitignore file to .npmignore
+  //
+  vfs.move(`${dest}/gitignore`, `${dest}/.gitignore`)
 }
 
 //
