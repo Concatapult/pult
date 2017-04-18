328d1abab5fd2f21b5c9392d7df23b88a6f8d003
Gilbert <gilbertbgarza@gmail.com>
2017-04-18 12:50:18 -0500

    Add foundational config, for high priority config.
    This allows setup such as typescript to always be in front,
    removing the potential for user errors.

--- base-template/server/config/index.js
+++ base-template/server/config/index.js
@@ -4,6 +4,18 @@ CONFIG.projectFile = (path) => Path.resolve(__dirname, '../..', path)
 
 
 module.exports = function config (router) {
+
+  //
+  // Foundational config should be seldom used,
+  // as it has a HIGH priority over the rest.
+  //
+  for (var item of (CONFIG.foundational || [])) {
+    require(item).mount(router)
+  }
+
+  //
+  // Run the rest of all configuration.
+  //
   for (var item of CONFIG.routerPipeline) {
     require(item).mount(router)
   }
